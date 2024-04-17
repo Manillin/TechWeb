a7d60119b15a52d0005bdf5818cbb2e54894eae8
Manillin <chri.vwm@gmail.com>
2024-04-17 14:44:58 +0200

    bug

--- django/biblio/gestione/views.py
+++ django/biblio/gestione/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Libro
 
-# Create your views here.
+
+def lista_libri(request):
+    templ = "gestione/listalibri.html"
+    ctx = {'title': "Lista Libri",
+           "listalibri": Libro.objects.all()}
+
+    return render(request, template_name=templ, context=ctx)
